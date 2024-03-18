af43267d141bd98997deb168809ff36e3ea0d34b
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-18 11:09:27 -0700

    Intro to Function Labs

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    console.log(string.toLowerCase());
+    if(sayHiToHeadphonedRoommate .lowerCase){
+        return "I can't hear you"
+    }else if(sayHiToHeadphonedRoommate .upperCase){
+        return "YES INDEED"
+    }else (string === `Let\'s have dinner together`);{
+        return "I would love to!"
+    }
+}
+
+
+  
+    
+    
+    
+
